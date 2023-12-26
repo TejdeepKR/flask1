e397aef548dbd49354bea1e7ab2cef265d4e52e2
TejdeepKR <tej2sandeep@gmail.com>
2023-12-26 08:38:03 +0530

    first commitsecond

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask,redirect,render_template,url_for
+from flask import Flask,redirect,render_template,url_for,request
 
 app = Flask(__name__)
 
 @app.route('/')
 def welcome():
     return 'Welcome'
+
+@app.route('/calculate',methods=['GET'])
+def math_operation():
+    opeartion= request.json["operation"]
+    num1 = request.json["num1"]
+    num2 = request.json["num2"]
+
+
+    if opeartion == 'add':
+        res = num1+num2
+    elif opeartion == 'sub':
+        res = num1 - num2
+    elif opeartion == 'div':
+        res= num1/num2
+    else:
+        res = num1 * num2
+
+    return res
+
+
 
 
 if __name__=='__main__':
